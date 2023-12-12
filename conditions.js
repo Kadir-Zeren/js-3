7af7427d5edcbbd2dcce8909b7e519c04a5403da
kadir-zeren <k4dzeren@gmail.com>
2023-12-12 11:39:52 +0300

    if

---
+++ conditions.js
@@ -0,0 +1,19 @@
+// * ===============================================
+// *                  KARAR YAPILARI
+// * ===============================================
+
+console.log("****** CONDITIONS *******");
+
+// const calismaSuresi = prompt("Calisma süresi:")
+// let maas = prompt("Maasini giriniz:")
+
+//? 2 kosullu
+// if (calismaSuresi >= 10) {
+//   // maas = Math.trunc(maas * 1.1)
+//   maas = Math.round(maas * 1.1)
+//   console.log("Zamli maasiniz:", maas)
+//   console.log(`Zamli maasiniz: ${maas}`)
+// } else {
+//   console.log("Uzgunuz maasinaza zam yapamayirouz", maas)
+// }
+// console.log("Güle güle")
