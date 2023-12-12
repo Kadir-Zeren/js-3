fa81f80db93f558aa50ef7c645bcc46b2a13dbd9
kadir-zeren <k4dzeren@gmail.com>
2023-12-12 11:50:46 +0300

    multi conditiın

--- conditions.js
+++ conditions.js
@@ -17,3 +17,39 @@ console.log("****** CONDITIONS *******");
 //   console.log("Uzgunuz maasinaza zam yapamayirouz", maas)
 // }
 // console.log("Güle güle")
+
+//? Tek kosullu
+// if (calismaSuresi >= 10) {
+//   maas = Math.round(maas * 1.1)
+//   console.log(`Zamli maasiniz: ${maas}`)
+// }
+// console.log("Güle güle")
+
+// //? Cok kosullu
+// const grade = prompt("Please enter your grade:")
+
+// let result = null
+// // let result = ""
+// if (grade < 0) {
+//   console.log("Grade can not be smaller than zero")
+// } else if (grade < 40) {
+//   result = "FF"
+// } else if (grade <= 50) {
+//   result = "DD"
+// } else if (grade <= 65) {
+//   result = "CC"
+// } else if (grade <= 89) {
+//   result = "BB"
+// } else if (grade <= 100) {
+//   result = "AA"
+// } else {
+//   console.log("Grade can not be bigger than 100")
+// }
+
+// if (result != null) {
+//   console.log(`Your score:${result}`)
+// }
+// //? Kısa yol
+// if (result) {
+//   console.log(`Your score:${result}`)
+// }
